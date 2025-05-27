fe16bb98e781dd182b2d6b14ac9fab24c4668ddd
Lauren <laurenpcaldwell@gmail.com>
2025-05-27 11:26:26 -0500

    tried search it ain't working

---
+++ webapp/static/search.js
@@ -0,0 +1,7 @@
+function getAPIBaseURL() {
+    let baseURL = window.location.protocol
+                    + '//' + window.location.hostname
+                    + ':' + window.location.port
+                    + '/api';
+    return baseURL;
+}

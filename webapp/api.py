39272395822266ada690ac4b1f7f18f9d41114ec
Tara <taradwright04@gmail.com>
2025-06-02 09:56:52 -0400

    api changes and home title is now home

--- webapp/api.py
+++ webapp/api.py
@@ -107,14 +107,23 @@ def get_pokemon_stats_by_name(pokemon_name):
         # Execute the query
         query = '''SELECT pokemon_type_stats.name, pokemon_type_stats.type1, pokemon_type_stats.type2, pokemon_type_stats.base_total, pokemon_type_stats.hp, pokemon_type_stats.attack, pokemon_type_stats.defense, pokemon_type_stats.sp_attack, pokemon_type_stats.sp_defense, pokemon_type_stats.speed
                     FROM pokemon_type_stats
-                    WHERE pokemon_type_stats.name = %s;'''
+                    WHERE pokemon_type_stats.name ILIKE %s;'''
         connection = get_connection()
         cursor = connection.cursor()
         cursor.execute(query, (pokemon_name,))
 
         # Iterate over the query results to produce the list of a given pokemon stats.
         for row in cursor:
-            pokemon_by_name.append({'Name':row[0], 'Type1':row[1], 'Type2':row[2], 'Total base stats': row[3], 'hp': row[4], 'attack': row[5], 'defense': row[6], 'sp attack': row[7], 'sp defense': row[8], 'speed': row[9]})
+            pokemon_by_name.append({'Name':row[0],
+                                    'Type1':row[1],
+                                    'Type2':row[2],
+                                    'Total base stats': row[3],
+                                    'hp': row[4],
+                                    'attack': row[5],
+                                    'defense': row[6],
+                                    'sp attack': row[7],
+                                    'sp defense': row[8],
+                                    'speed': row[9]})
 
         cursor.close()
         connection.close()
@@ -172,7 +181,7 @@ def get_pokemon_strength_and_weakness(pokemon_name):
         query = '''SELECT pokemon_strong.strong1, pokemon_strong.strong2, pokemon_strong.strong3, pokemon_strong.strong4, pokemon_strong.strong5, pokemon_weak.weak1, pokemon_weak.weak2, pokemon_weak.weak3, pokemon_weak.weak4, pokemon_weak.weak5
         FROM pokemon_strong, pokemon_type_stats, pokemon_weak
         WHERE pokemon_strong.type = pokemon_weak.type
-        AND pokemon_type_stats.name = %s
+        AND pokemon_type_stats.name ILIKE %s
         AND (pokemon_type_stats.type1 = pokemon_strong.type
         OR pokemon_type_stats.type2 = pokemon_strong.type)
         AND (pokemon_type_stats.type1 = pokemon_weak.type
@@ -182,7 +191,16 @@ def get_pokemon_strength_and_weakness(pokemon_name):
 
         # Iterate over the query results to produce the list of a given pokemon stats.
         for row in cursor:
-            pokemon_strength_and_weakness.append({'strong1':row[0], 'strong2':row[1], 'strong3':row[2], 'strong4': row[3], 'strong5': row[4], 'weak1': row[5], 'weak2': row[6], 'weak3': row[7], 'weak4': row[8], 'weak5': row[9]})
+            pokemon_strength_and_weakness.append({'strong1':row[0], 
+                                                  'strong2':row[1], 
+                                                  'strong3':row[2], 
+                                                  'strong4': row[3], 
+                                                  'strong5': row[4], 
+                                                  'weak1': row[5], 
+                                                  'weak2': row[6], 
+                                                  'weak3': row[7], 
+                                                  'weak4': row[8], 
+                                                  'weak5': row[9]})
 
         cursor.close()
         connection.close()
@@ -204,7 +222,7 @@ def get_pokemon_class(pokemon_name):
         # Execute the query
         query = '''SELECT pokemon_class_and_gender.pokemon_class
                     FROM pokemon_class_and_gender
-                    WHERE pokemon_class_and_gender.name = %s;'''
+                    WHERE pokemon_class_and_gender.name ILIKE %s;'''
     
         cursor.execute(query, (pokemon_name,))
 
@@ -232,7 +250,7 @@ def get_pokemon_gender(pokemon_name):
         # Execute the query
         query = '''SELECT pokemon_class_and_gender.gender
                     FROM pokemon_class_and_gender
-                    WHERE pokemon_class_and_gender.name = %s;'''
+                    WHERE pokemon_class_and_gender.name ILIKE %s;'''
     
         cursor.execute(query, (pokemon_name,))
 
